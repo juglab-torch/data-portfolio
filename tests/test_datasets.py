d0d68803f3adfb34d26387bb1198e471a716f0ed
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2023-04-09 21:04:58 +0200

    Add tests

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -3,16 +3,6 @@ import pytest
 from data_portfolio import Portfolio
 
 
-def test_faulty_md5(tmp_path, faulty_portfolio_entry):
-    """Test that faulty md5 hash raises an error."""
-
-    # test that it downloads fine when not checking md5 hash
-    faulty_portfolio_entry.download(tmp_path, check_md5=False)
-    assert (tmp_path / faulty_portfolio_entry.file_name).exists()
-
-    with pytest.raises(ValueError):
-        faulty_portfolio_entry.download(tmp_path, check_md5=True)
-
 @pytest.mark.dataset
 def test_n2v_bsd68(tmp_path):
     """Test that N2V_BSD68 dataset downloads properly."""
--- tests/test_portfolio_entry.py
+++ tests/test_portfolio_entry.py
@@ -1,5 +1,17 @@
+import pytest
+import hashlib
 
-# TODO test that we can download the datasets
 
-def test_something():
-    pass
+def test_faulty_md5(tmp_path, faulty_portfolio_entry):
+    """Test that faulty md5 hash raises an error."""
+
+    # test that it downloads fine when not checking md5 hash
+    faulty_portfolio_entry.download(tmp_path, check_md5=False)
+    assert (tmp_path / faulty_portfolio_entry.file_name).exists()
+
+    with pytest.raises(ValueError):
+        faulty_portfolio_entry.download(tmp_path, check_md5=True)
+
+
+def test_change_entry(faulty_portfolio_entry):
+    faulty_portfolio_entry.md5_hash = hashlib.md5(b"Malicious file hash").hexdigest()
