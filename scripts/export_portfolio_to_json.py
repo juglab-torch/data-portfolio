db0e23191a7f16332f3fcea1c988e5b4f86885d9
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2023-04-12 10:15:31 +0200

    Auto-commit json action

--- scripts/export_portfolio_to_json.py
+++ scripts/export_portfolio_to_json.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     portfolio = Portfolio()
 
     # Path to the datasets list
-    path_to_datasets = Path(__file__).parent.parent / "datasets" / "datasets.json"
+    path_to_datasets = Path(".", "datasets", "datasets.json")
 
     # Export the portfolio to json
     portfolio.to_json(path_to_datasets)
