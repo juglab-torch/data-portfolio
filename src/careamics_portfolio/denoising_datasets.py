2c11c8644cee2595c3f721ff2bb1e48550177cb8
CatEek <zubarev.ia@gmail.com>
2023-11-28 16:39:55 +0100

    n2n sem

--- src/careamics_portfolio/denoising_datasets.py
+++ src/careamics_portfolio/denoising_datasets.py
@@ -3,6 +3,44 @@ from .portfolio_entry import PortfolioEntry
 DENOISING = "denoising"
 
 
+class N2N_SEM(PortfolioEntry):
+    """SEM dataset.
+
+    Attributes
+    ----------
+        portfolio (str): Name of the portfolio to which the dataset belong.
+        name (str): Name of the dataset.
+        url (str): URL of the dataset.
+        description (str): Description of the dataset.
+        license (str): License of the dataset.
+        citation (str): Citation to use when referring to the dataset.
+        file_name (str): Name of the downloaded file.
+        hash (str): SHA256 hash of the downloaded file.
+        files (list[str]): List of files in the dataset.
+        size (int): Size of the dataset in MB.
+        tags (list[str]): List of tags associated to the dataset.
+        is_zip (bool): Whether the dataset is a zip file.
+    """
+
+    def __init__(self) -> None:
+        super().__init__(
+            portfolio=DENOISING,
+            name="N2N_SEM",
+            url="https://download.fht.org/jug/n2n_sem_data/n2n_sem_data.zip",
+            file_name="n2n_sem_data.zip",
+            sha256="cffe262efaa30e99129268d6eb9399c20417333b77fbdb82abf661a8d42b9551",
+            description="SEM dataset from T.-O. Buchholz et al "
+            "(Methods Cell Biol, 2020).",
+            license="CC-BY-4.0",
+            citation="T.-O. Buchholz, A. Krull, R. Shahidi, G. Pigino, G. Jékely, "
+            'F. Jug, "Content-aware image restoration for electron '
+            'microscopy", Methods Cell Biol 152, 277-289',
+            files=["train.tif", "validation.tif"],
+            size=167.0,
+            tags=["denoising", "electron microscopy"],
+        )
+
+
 class N2V_BSD68(PortfolioEntry):
     """BSD68 dataset.
 
